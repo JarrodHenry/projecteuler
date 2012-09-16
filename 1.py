545e1c75faa8fd784dd1bd131c57b8552acc6f1d
Jarrod Henry <jarrodhenry@gmail.com>
2012-09-16 10:50:49 -0500

    derp.  No need for that list

--- 1.py
+++ 1.py
@@ -1,12 +1,8 @@
 
 target = range(1,1000)
-summation = []
 total = 0
 for number in target:
 	if number % 3 == 0 or number % 5 == 0:
-		summation.append(number)
-
-for number in summation:
-	total = total + number
+		total = total + number
 
 print total	
