112e990b7b1a9e0c6fc900c8e5606fdd8f964d07
Jarrod Henry <jarrodhenry@gmail.com>
2012-09-17 09:39:44 -0500

    made a sieve for this one, thanks python cookbook

---
+++ 3.py
@@ -0,0 +1,21 @@
+from __future__ import generators
+
+
+def eratosthenes(maxnum):
+	D = {}  # map composite integers to primes witnessing their compositeness
+	q = 2   # first integer to test for primality
+	while q <= maxnum:
+		if q not in D:
+			yield q       # not marked composite, must be prime
+			D[q*q] = [q]  # first multiple of q not already marked
+		else:
+			for p in D[q]:  # move each witness to its next multiple
+				D.setdefault(p+q,[]).append(p)
+			del D[q]        # no longer need D[q], free memory
+		q += 1
+
+
+for p in eratosthenes(600851475143):
+	if 600851475143 % p == 0:
+		print p
+ 
