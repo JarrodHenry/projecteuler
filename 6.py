f25bcbd31c2e43edc185ddab117fd8da1d77a5e0
Jarrod Henry <jarrodhenry@gmail.com>
2012-09-18 10:58:37 -0500

    problem 6

---
+++ 6.py
@@ -0,0 +1,16 @@
+
+
+squareofsum = sum(range(1,101))**2
+sumofsquares = 0
+
+print squareofsum
+
+for number in range(1,101):
+	sumofsquares= sumofsquares + number**2
+
+
+print sumofsquares
+
+print squareofsum - sumofsquares
+
+
